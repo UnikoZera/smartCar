1a8d6097b72ab83144f56656a059bfb62e6620f9
UnikoZera <3864437775@qq.com>
2024-11-22 10:32:45 +0800

    bugfix

--- main.c
+++ main.c
@@ -3,30 +3,13 @@
 #include "carMovement.h"
 #include "carTracking.h"
 
-void Delay1000ms(void)	//@11.0592MHz
-{
-	unsigned char data i, j, k;
-
-	_nop_();
-	i = 8;
-	j = 1;
-	k = 243;
-	do
-	{
-		do
-		{
-			while (--k);
-		} while (--j);
-	} while (--i);
-}
-
 void main()
 {
 	Timer0_Init();
 	Timer1_Init();
 	while(1)
 	{
-		// Tracking();
-		carStop();
+		Tracking();
+		// carStop();
 	}
 }
